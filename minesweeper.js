9fc3a12e5b1e0877d4975ebe511818cb4bc49e56
AhmedShab <ahmed.vuw@gmail.com>
2016-06-21 11:50:37 +1200

    finished the getRow function

--- minesweeper.js
+++ minesweeper.js
@@ -1,15 +1,35 @@
 document.addEventListener('DOMContentLoaded', startGame);
 
+var board = {
+  cells : [
+
+  ]
+};
+
 function startGame () {
   var board = document.getElementsByClassName('board')[0].children;
   for (var i = 0; i < board.length; i++) {
     addListeners(board[i]);
+    getRow(board[i]);
   }
 }
 
 function addListeners(elem) {
   elem.addEventListener('click', showCell);
   elem.addEventListener('contextmenu', markCell);
+}
+
+function getRow(ele) {
+  var className = ele.classList;
+  for (var i = 0; i < className.length; i++) {
+    // console.log(className[i]);
+    if (className[i].indexOf("row-") > -1) {
+      // console.log(className[i]);
+       return (className[i].split("row-").join(""));
+    }
+  }
+
+  // console.log(row[1]);
 }
 
 function showCell(event) {
