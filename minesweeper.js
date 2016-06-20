d4c07b6d5491adba43f6a2fd48227ba6c71639a3
AhmedShab <ahmed.vuw@gmail.com>
2016-06-21 10:55:58 +1200

    you can now remove the class hidden from each ele

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', startGame)
+document.addEventListener('DOMContentLoaded', startGame);
 
 function startGame () {
+  var board = document.getElementsByClassName('board')[0].children;
+  for (var i = 0; i < board.length; i++) {
+    addListeners(board[i]);
+  }
+}
+
+function addListeners(elem) {
+  elem.addEventListener('click', showCell);
+}
+
+function showCell(event) {
+  event.target.classList.remove('hidden');
 }
--- minesweeper_lib.js
+++ minesweeper_lib.js
@@ -2,12 +2,12 @@
 // Returns a subset of the `cells` array, including only those cells
 // which are adjacent to `row`, `col`
 function getSurroundingCells (row, col) {
-  var columns = getRange(getLowerBound(col), getUpperBound(col))
-  var rows = getRange(getLowerBound(row), getUpperBound(row))
+  var columns = getRange(getLowerBound(col), getUpperBound(col));
+  var rows = getRange(getLowerBound(row), getUpperBound(row));
   return board.cells
     .filter(function (cell) {
-      return columns.includes(cell.col) && rows.includes(cell.row)
-    })
+      return columns.includes(cell.col) && rows.includes(cell.row);
+    });
 }
 
 // For the given DOM element, displays surrounding mine counts
@@ -17,13 +17,13 @@ function getSurroundingCells (row, col) {
 function showSurrounding (element) {
   getSurroundingCells(getRow(element), getCol(element))
     .filter(function (cell) {
-      return !cell.isMine
+      return !cell.isMine;
     })
     .filter(function (cell) {
       // Avoid breaking the call stack with recurrent checks on same cell
-      return !cell.isProcessed
+      return !cell.isProcessed;
     })
-    .forEach(setInnerHTML)
+    .forEach(setInnerHTML);
 }
 
 // For the given cell object, set innerHTML to cell.surroundingMines
@@ -33,22 +33,22 @@ function showSurrounding (element) {
 // If surroundingMines is 0, greedily attempt to expose as many more cells
 // as possible.
 function setInnerHTML (cell) {
-  cell.isProcessed = true
-  var rowClass = 'row-' + cell.row
-  var colClass = 'col-' + cell.col
-  var element = document.getElementsByClassName(rowClass + ' ' + colClass)[0]
+  cell.isProcessed = true;
+  var rowClass = 'row-' + cell.row;
+  var colClass = 'col-' + cell.col;
+  var element = document.getElementsByClassName(rowClass + ' ' + colClass)[0];
   if (element.innerHTML !== '') {
-    return
+    return;
   }
   if (element.classList.contains('marked')) {
-    return
+    return;
   }
-  element.innerHTML = cell.surroundingMines > 0 ? 
-    cell.surroundingMines : ''
+  element.innerHTML = cell.surroundingMines > 0 ?
+    cell.surroundingMines : '';
   if (element.classList.contains('hidden')) {
-    element.classList.remove('hidden')
+    element.classList.remove('hidden');
     if (cell.surroundingMines === 0) {
-      return showSurrounding(element)
+      return showSurrounding(element);
     }
   }
 }
@@ -56,15 +56,15 @@ function setInnerHTML (cell) {
 function getRange(begin, end) {
   return Array.apply(begin, Array(end - begin + 1))
     .map(function (n, i) {
-      return begin + i
-    })
+      return begin + i;
+    });
 }
 
 function getLowerBound (n) {
-  return n - 1 < 0 ? 0 : n -1
+  return n - 1 < 0 ? 0 : n -1;
 }
 
 function getUpperBound (n) {
-  var limit = board.MAX_CELLS-1 || 4
-  return n + 1 > limit ? limit : n + 1
+  var limit = board.MAX_CELLS-1 || 4;
+  return n + 1 > limit ? limit : n + 1;
 }
